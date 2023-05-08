d7739445c059489987996f98299bb8a63ac81457
Ansh Mondal <ansh1mondal@gmail.com>
2023-05-08 14:15:37 +0530

    folders created

---
+++ src/jsxFiles/Waste.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Waste() {
+  return (
+    <>
+      <div className="Acontainer">
+        <div className="Abody"></div>
+      </div>
+    </>
+  );
+}
