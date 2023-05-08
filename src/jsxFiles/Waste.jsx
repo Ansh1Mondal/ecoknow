9056ef6eb5751e7873a8e78baedf86915b066bb6
Ansh Mondal <ansh1mondal@gmail.com>
2023-05-08 14:36:28 +0530

    logo added

--- src/jsxFiles/Waste.jsx
+++ src/jsxFiles/Waste.jsx
@@ -1,10 +1,16 @@
 import React from "react";
+import "../cssFiles/Waste.css";
 
 export default function Waste() {
   return (
     <>
       <div className="Acontainer">
-        <div className="Abody"></div>
+        <div className="Abody">
+          <div className="Aradio">
+            <h1>Waste management</h1>
+          </div>
+          <div className="Amap"></div>
+        </div>
       </div>
     </>
   );
