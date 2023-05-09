7bd6a98dcb52518e018b7dd97c919e340f8723de
Ansh Mondal <ansh1mondal@gmail.com>
2023-05-09 11:28:36 +0530

    animation

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 // import logo from './logo.svg';
 import "./App.css";
 // import Waste from "../src/jsxFiles/Waste";
-// import Home from "../src/jsxFiles/Home";
-import About from "../src/jsxFiles/About";
+import Home from "../src/jsxFiles/Home";
+// import About from "../src/jsxFiles/About";
 
 function App() {
-  return <About />;
+  return <Home />;
 }
 
 export default App;
--- src/jsxFiles/Home.jsx
+++ src/jsxFiles/Home.jsx
@@ -6,16 +6,17 @@ export default function Home() {
   return (
     <>
       <div className="Pcontainer">
-        <div className="Pnavbar">
-            Navbar
-        </div>
+        <div className="Pnavbar">Navbar</div>
         <div className="Pbody">
           <div className="Pleft">
             <div className="Ptext">EcoKnow</div>
+            <div className="Ptag">
+              <p>CLEAN CITY . GREEN CITY</p>
+            </div>
             <button className="Pbutton">Let's Explore!</button>
           </div>
           <div className="Pright">
-            <img src={Logo} alt="Logo" className="Plogo"/>
+            <img src={Logo} alt="Logo" className="Plogo" />
           </div>
         </div>
       </div>
